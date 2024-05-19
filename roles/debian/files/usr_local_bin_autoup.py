4605f04c7dcb08197cc8ccb508beafa376838cff
MephistoaD <MephistoaD>
2024-05-19 20:15:03 +0200

    feat(debian): allow optional reason for locked autoup

--- roles/debian/files/usr_local_bin_autoup.py
+++ roles/debian/files/usr_local_bin_autoup.py
@@ -6,25 +6,26 @@ import os
 PROM_FILE = '/var/lib/prometheus/node-exporter/autoup.prom'
 LOCKED_CONTENT = """# HELP node_exporter_autoup_locked State of last maintenance job on the machine (0 = unlocked, 1 = locked)
 # TYPE node_exporter_autoup_locked gauge
-node_exporter_autoup_locked 1
+node_exporter_autoup_locked{{origin="{reason}"}} 1
 """
 UNLOCKED_CONTENT = """# HELP node_exporter_autoup_locked State of last maintenance job on the machine (0 = unlocked, 1 = locked)
 # TYPE node_exporter_autoup_locked gauge
-node_exporter_autoup_locked 0
+node_exporter_autoup_locked{{origin=""}} 0
 """
 
 def print_help():
     print("Usage:")
-    print("  autoup lock      Locking machine for future maintenances")
-    print("  autoup unlock    Unlocking machine for future maintenances")
-    print("  autoup           Returns the content of /var/lib/prometheus/node-exporter/autoup.prom")
-    print("  autoup --help    Prints this help message")
-    print("  autoup -h        Prints this help message")
+    print("  autoup lock [reason]   Locking machine for future maintenances with an optional reason")
+    print("  autoup unlock          Unlocking machine for future maintenances")
+    print("  autoup                 Returns the content of /var/lib/prometheus/node-exporter/autoup.prom")
+    print("  autoup --help          Prints this help message")
+    print("  autoup -h              Prints this help message")
 
-def lock_machine():
-    print("Locking machine for future maintenances...")
+def lock_machine(reason=""):
+    print(f"Locking machine for future maintenances... Reason: {reason}")
+    content = LOCKED_CONTENT.format(reason=reason)
     with open(PROM_FILE, 'w') as f:
-        f.write(LOCKED_CONTENT)
+        f.write(content)
     print_content()
 
 def unlock_machine():
@@ -43,11 +44,12 @@ def print_content():
 def main():
     if len(sys.argv) == 1:
         print_content()
-    elif len(sys.argv) == 2:
+    elif len(sys.argv) >= 2:
         if sys.argv[1] in ['--help', '-h']:
             print_help()
         elif sys.argv[1] == 'lock':
-            lock_machine()
+            reason = " ".join(sys.argv[2:]) if len(sys.argv) > 2 else ""
+            lock_machine(reason)
         elif sys.argv[1] == 'unlock':
             unlock_machine()
         else:
