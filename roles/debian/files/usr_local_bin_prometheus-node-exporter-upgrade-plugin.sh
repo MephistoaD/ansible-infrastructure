0f184dd7eddd2a2dd7f7f20fcd083555d2614aad
MephistoaD <mephistoad@github.com>
2024-07-22 14:42:21 +0200

    fix(debian): prevent plugin from exporting none as upgradable package

--- roles/debian/files/usr_local_bin_prometheus-node-exporter-upgrade-plugin.sh
+++ roles/debian/files/usr_local_bin_prometheus-node-exporter-upgrade-plugin.sh
@@ -7,15 +7,18 @@ UPGRADABLE_PACKAGES=$(apt-get -s --no-download dist-upgrade -V | grep '=>')
 
 echo "# upgradable packages" > $TEMPFILE
 
-# Process each line in UPGRADABLE_PACKAGES
-while IFS= read -r line; do
-    # Extract the package name
-    package=$(echo "$line" | awk '{print $1}')
-    # Trim leading and trailing spaces and escape double quotes in the line for Prometheus syntax
-    escaped_line=$(echo "$line" | sed 's/"/\\"/g' | sed 's/^ *//;s/ *$//')
-    # Append the line to the TEMPFILE
-    echo "upgradable_package{package=\"$package\",origin=\"$escaped_line\"} 1" >> $TEMPFILE
-done <<< "$UPGRADABLE_PACKAGES"
+# Check if there are any pending upgrades
+if [ -n "$UPGRADABLE_PACKAGES" ]; then
+    # Process each line in UPGRADABLE_PACKAGES
+    while IFS= read -r line; do
+        # Extract the package name
+        package=$(echo "$line" | awk '{print $1}')
+        # Trim leading and trailing spaces and escape double quotes in the line for Prometheus syntax
+        escaped_line=$(echo "$line" | sed 's/"/\\"/g' | sed 's/^ *//;s/ *$//')
+        # Append the line to the TEMPFILE
+        echo "upgradable_package{package=\"$package\",origin=\"$escaped_line\"} 1" >> $TEMPFILE
+    done <<< "$UPGRADABLE_PACKAGES"
+fi
 
 # Move the tempfile to the exporter directory
 mv $TEMPFILE $EXPORTER_DIR
