030b5c8ec3271c547bcdaa31524d88b2cfe4161d
MephistoaD <mephistoad@github.com>
2024-07-27 10:19:51 +0200

    fix(dbebian): make upgrade exporter plugin ignore held packages

--- roles/debian/files/usr_local_bin_prometheus-node-exporter-upgrade-plugin.sh
+++ roles/debian/files/usr_local_bin_prometheus-node-exporter-upgrade-plugin.sh
@@ -4,6 +4,7 @@ TEMPFILE=/tmp/upgradable_packages.prom
 EXPORTER_DIR=/var/lib/prometheus/node-exporter/
 
 UPGRADABLE_PACKAGES=$(apt-get -s --no-download dist-upgrade -V | grep '=>')
+HOLD_PACKAGES=$(apt-mark showhold)
 
 echo "# upgradable packages" > $TEMPFILE
 
@@ -13,10 +14,14 @@ if [ -n "$UPGRADABLE_PACKAGES" ]; then
     while IFS= read -r line; do
         # Extract the package name
         package=$(echo "$line" | awk '{print $1}')
-        # Trim leading and trailing spaces and escape double quotes in the line for Prometheus syntax
-        escaped_line=$(echo "$line" | sed 's/"/\\"/g' | sed 's/^ *//;s/ *$//')
-        # Append the line to the TEMPFILE
-        echo "upgradable_package{package=\"$package\",origin=\"$escaped_line\"} 1" >> $TEMPFILE
+
+        # Check if the package is not in the HOLD_PACKAGES list
+        if ! echo "$HOLD_PACKAGES" | grep -qw "$package"; then
+            # Trim leading and trailing spaces and escape double quotes in the line for Prometheus syntax
+            escaped_line=$(echo "$line" | sed 's/"/\\"/g' | sed 's/^ *//;s/ *$//')
+            # Append the line to the TEMPFILE
+            echo "upgradable_package{package=\"$package\",origin=\"$escaped_line\"} 1" >> $TEMPFILE
+        fi
     done <<< "$UPGRADABLE_PACKAGES"
 fi
 
